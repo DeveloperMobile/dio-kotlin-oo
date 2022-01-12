ab85abb2339ccf9915e13d91abfcac10da8f5da2
Tiago <tiagopereiravieira@gmail.com>
2022-01-12 11:40:12 -0300

    update to apply the private modifier to set function of cpf propert

--- digioebank/src/main/kotlin/digital/innovation/one/Pessoa.kt
+++ digioebank/src/main/kotlin/digital/innovation/one/Pessoa.kt
@@ -3,15 +3,11 @@ package digital.innovation.one
 class Pessoa {
     var nome: String = "Tiago"
     var cpf: String = "123.123.123-12"
-
-    inner class Endereco {
-        var rua: String = "Rua Teste"
-    }
+    private set
 }
 
 fun main() {
     var pessoa = Pessoa()
     println(pessoa.nome)
     println(pessoa.cpf)
-    println(pessoa.Endereco().rua)
 }
