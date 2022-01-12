6913095026be4b4ba8827ade78a9c8a70ba4fb5c
Tiago <tiagopereiravieira@gmail.com>
2022-01-12 11:48:02 -0300

    updated to talk about class members

--- digioebank/src/main/kotlin/digital/innovation/one/Pessoa.kt
+++ digioebank/src/main/kotlin/digital/innovation/one/Pessoa.kt
@@ -4,10 +4,13 @@ class Pessoa {
     var nome: String = "Tiago"
     var cpf: String = "123.123.123-12"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     var pessoa = Pessoa()
-    println(pessoa.nome)
-    println(pessoa.cpf)
+    println(pessoa.pessoaInfo())
 }
