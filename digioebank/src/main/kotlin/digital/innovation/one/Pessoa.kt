9e284e0dc146c2c1f7be14ce75e0e0787ff87278
Tiago <tiagopereiravieira@gmail.com>
2022-01-12 11:29:31 -0300

    inner class Endereco

--- digioebank/src/main/kotlin/digital/innovation/one/Pessoa.kt
+++ digioebank/src/main/kotlin/digital/innovation/one/Pessoa.kt
@@ -1,4 +1,17 @@
 package digital.innovation.one
 
 class Pessoa {
+    var nome: String = "Tiago"
+    var cpf: String = "123.123.123-12"
+
+    inner class Endereco {
+        var rua: String = "Rua Teste"
+    }
+}
+
+fun main() {
+    var pessoa = Pessoa()
+    println(pessoa.nome)
+    println(pessoa.cpf)
+    println(pessoa.Endereco().rua)
 }
